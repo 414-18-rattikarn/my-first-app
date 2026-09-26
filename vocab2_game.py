ec1bc660fff1d4d6acbafb511aeba095a85abeef
414-18-rattikarn <54557@yupparaj.ac.th>
2026-09-26 10:39:20 +0700

    Update vocab2_game.py

--- vocab2_game.py
+++ vocab2_game.py
@@ -93,8 +93,8 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
         st.session_state.is_ended = True
         st.rerun()
 
-   time.sleep(1)
-   st.rerun()
+    time.sleep(1)
+    st.rerun()
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
